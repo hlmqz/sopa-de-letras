33222c069483de184826de54a235a1d3d7325f4a
Hemerson Marquez <hlmarquezm@gmail.com>
2019-03-10 21:01:04 -0500

    Inicialización

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import '@fortawesome/fontawesome-free/js/all.min.js'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'animate.css'
+
 Vue.config.productionTip = false
 
 new Vue({
